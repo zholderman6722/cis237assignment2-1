fc2ee4817e7d0a84224b77c099ee7de2eba7910e
David Barnes <dbarnes@kvcc.edu>
2015-09-29 23:15:07 -0400

    Fix error related to the order in which the solve method is called in relation to the transpose method.

--- cis237assignment2/Program.cs
+++ cis237assignment2/Program.cs
@@ -46,13 +46,13 @@ namespace cis237assignment2
             /// </summary>
             MazeSolver mazeSolver = new MazeSolver();
 
+            //Create the second maze by transposing the first maze
+            char[,] maze2 = transposeMaze(maze1);
+
             /// <summary>
             /// Tell the instance to solve the first maze with the passed maze, and start coordinates.
             /// </summary>
             mazeSolver.SolveMaze(maze1, X_START, Y_START);
-
-            //Create the second maze by transposing the first maze
-            char[,] maze2 = transposeMaze(maze1);
 
             //Solve the transposed maze.
             mazeSolver.SolveMaze(maze2, X_START, Y_START);
